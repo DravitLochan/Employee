d70905d00613a06878f2405a9bc6f70b819dd48d
DravitLochan <dlochangupta@gmail.com>
2017-03-20 20:41:47 +0530

    dashboard prepared!

--- app/src/main/java/ims/com/employee/Helpers/Validate.java
+++
@@ -1,9 +0,0 @@
-package ims.com.employee.Helpers;
-
-/**
- * Created by DravitLochan on 16-03-2017.
- */
-
-public class Validate {
-
-}
--- app/src/main/java/ims/com/employee/MainActivity.java
+++ app/src/main/java/ims/com/employee/MainActivity.java
@@ -1,10 +1,17 @@
 package ims.com.employee;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 import ims.com.employee.prefs.UserCreds;
 
@@ -12,21 +19,27 @@ public class MainActivity extends AppCompatActivity {
 
     Context context;
     UserCreds userCreds;
+    int hours,min;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final AlertDialog.Builder altr=new AlertDialog.Builder(MainActivity.this);
+
         context=this;
         userCreds = new UserCreds(context);
-
         if(!userCreds.getIsUserSet())
         {
             startActivity(new Intent(MainActivity.this,Login.class));
             finish();
         }
+
         else
         {
             Toast.makeText(context,"welcome "+userCreds.getUser().getEmail(),Toast.LENGTH_SHORT).show();
+
         }
     }
+
 }
